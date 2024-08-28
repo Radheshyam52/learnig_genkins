f8ae03bd2c6d6c70951210d3d1e08e18e20059cf
radheshyam <shyam25jio@gmail.com>
2024-08-28 09:20:55 +0530

    learning about email notification

---
+++ index.py
@@ -0,0 +1,11 @@
+# Import the datetime module
+from datetime import datetime
+
+# Print a greeting message
+print("Hello, World!")
+
+# Get the current date and time
+now = datetime.now()
+
+# Print the current date and time
+print("Current date and time:", now.strftime("%Y-%m-%d %H:%M:%S"))
